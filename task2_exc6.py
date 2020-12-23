65eef2f873dd4287adb3a9af50b957756b1f7960
Krathee <kanikarathee015@gmail.com>
2020-12-22 21:06:00 -0800

    Added exercise 6 of task two

---
+++ task2_exc6.py
@@ -0,0 +1,35 @@
+print ("exercise 6 part one")
+
+'''x = 123
+for i in x:
+    print(i)'''
+
+print ("It shows error as x is int cann't take place of range")
+
+print("\n exercise 6 part two")
+
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+    if i == 3:
+        break
+    else:
+        print ("Error")    
+
+output = "0 Error 1 Error 2"
+
+print("\n exercise 6 part three")
+
+count = 0
+
+while True:
+    print(count)
+
+    count +=1
+
+    if count >= 5:
+        break
+
+# It will print the numbers from 0 to 4 and after that it will break as if statement will be true
