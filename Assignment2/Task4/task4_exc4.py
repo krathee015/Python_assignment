eb9dc78cb18550f79e9381640189f09277cb87f9
Krathee <kanikarathee015@gmail.com>
2020-12-27 20:29:00 -0800

    Added exercise 4 and 5

---
+++ Assignment2/Task4/task4_exc4.py
@@ -0,0 +1,7 @@
+print("Exercise 4")
+print("hyphen-separated sequence of words as input and prints the words in a hyphen-separated sequence after sorting them alphabetically")
+
+str = input("Enter a hyphen-seperated sequence: ")
+
+def sorted_seq (seq1):
+        
---
+++ Assignment2/Task4/task4_exc5.py
@@ -0,0 +1,6 @@
+print("a sequence of lines as input and prints the lines after making all characters in the sentence capitalized.")
+
+str1 = map(lambda i:i.upper(), ['Hello', 'everyone'])
+a = list(str1)
+print (' '.join(a))
+
