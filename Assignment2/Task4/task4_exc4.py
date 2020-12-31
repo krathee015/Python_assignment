68e4d678c120b3cc76f4f7ebcf999848e4572ab3
Krathee <kanikarathee015@gmail.com>
2020-12-31 13:17:55 -0800

    Added exercise 4

--- Assignment2/Task4/task4_exc4.py
+++ Assignment2/Task4/task4_exc4.py
@@ -1,7 +1,12 @@
 print("Exercise 4")
 print("hyphen-separated sequence of words as input and prints the words in a hyphen-separated sequence after sorting them alphabetically")
 
-str = input("Enter a hyphen-seperated sequence: ")
-
 def sorted_seq (seq1):
+
+    x = seq1.split('-')
+    x.sort()
+    print("After sorting: ",'-'.join(x))
+
+str = input("Enter a hyphen-seperated sequence: ")
+sorted_seq(str)
         
