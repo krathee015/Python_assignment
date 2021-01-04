e44428ad343e0ff2f21cee7011d042b6200a9973
Krathee <kanikarathee015@gmail.com>
2021-01-03 22:15:53 -0600

    added exc 12

---
+++ Assignment2/Task4/task4_exc12.py
@@ -0,0 +1,10 @@
+print("Write a function to compute 5/0 and use try/except to catch the exceptions")
+
+def fun():
+    
+    try:
+        x = 5/0
+    except:
+        print("It is an error")
+
+fun()
