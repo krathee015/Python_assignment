b274dd4d6f0586a3677d73ef8178b04b981a360f
Krathee <kanikarathee015@gmail.com>
2021-01-03 21:54:55 -0600

    added exc 11

---
+++ Assignment2/Task4/task4_exc11.py
@@ -0,0 +1,7 @@
+print("map() and filter() to make a list whose elements are squares of even numbers")
+
+listUse = [1,2,3,4,5,6,7,8,9,10]
+
+even_num = map(lambda x: x**2, filter(lambda x: x%2 == 0, listUse))
+
+print(list(even_num))
