b0dbc0d578624b9012d16fc9b89ce63c8916f6d7
Krathee <kanikarathee015@gmail.com>
2021-01-04 14:41:50 -0600

    Changed print into comments

--- Assignment2/task3.py
+++ Assignment2/task3.py
@@ -1,8 +1,6 @@
-from typing import Sequence
-
 
 print ("Exercise 1")
-print ("Create a list of 10 elements of four different data types like int, string, complex and float")
+# Create a list of 10 elements of four different data types like int, string, complex and float
 
 complex_number = 5.5j
 list_display = [1, 5, 4.4, complex_number, "KR", "DL", 7, 7.7, 9, "IN"]
@@ -11,14 +9,14 @@ print(list_display)
 
 print("\n*****")
 print("Exercise 2")
-print ("Create a list of size 5 and execute the slicing structure ")
+# Create a list of size 5 and execute the slicing structure 
 
 list= [1,5,8,9,6]
 print("Slicing the list: ", list[0:3])
 
 print("\n*****")
 print("Exercise 3")
-print (" Write a program to get the sum and multiply of all the items in a given list.")
+# Write a program to get the sum and multiply of all the items in a given list.
 
 print ("Sum of all the numbers in the list: ", sum(list))
 
@@ -37,7 +35,7 @@ print ("Smallest number in the given list is: ", min(list))
 
 print("\n*****")
 print("Exercise 5")
-print ("Create a new list which contains the specified numbers after removing the even numbers from a predefined list.")
+# Create a new list which contains the specified numbers after removing the even numbers from a predefined list.
 
 list1 = [4,6,3,8,9,5,13,45,56,79]
 new_list = [ ]
@@ -48,7 +46,7 @@ print ("List after removing even numbers is: ",new_list)
 
 print("\n*****")
 print("Exercise 6")
-print("A list of elements such that it contains the squares of the first and last 5 elements between 1 and 30(both included)")
+# A list of elements such that it contains the squares of the first and last 5 elements between 1 and 30(both included)
 list2 = []
 for i in range(1,31):
     if (i in range(1,6) or i in range(25,31)):
@@ -58,7 +56,7 @@ print("List: ", list2)
 
 print("\n*****")
 print("Exercise 7")
-print("Write a program to replace the last element in a list with another list")
+# Write a program to replace the last element in a list with another list
 
 main_list = [1,2,3,4,5,6]
 replace_list =[10,11,12]
@@ -67,7 +65,7 @@ print("After replacing last value with the new list: ", main_list)
 
 print("\n*****")
 print("Exercise 8")
-print("Create a new dictionary by concatenating the following two dictionaries:")
+# Create a new dictionary by concatenating the following two dictionaries:
 dict1 ={1:20, 2:20}
 dict2 ={3:30, 4:40}
 dict1.update(dict2)
@@ -75,7 +73,7 @@ print ("New dictionary after concatenation: ", dict1)
 
 print("\n*****")
 print("Exercise 9")
-print("dictionary that contain numbers in the form(x:x*x) where x takes all the values between 1 and n(both 1 and n included)")
+# dictionary that contain numbers in the form(x:x*x) where x takes all the values between 1 and n(both 1 and n included)
 
 n = eval(input("Enter the value for n: "))
 dict1 = dict()
@@ -85,7 +83,7 @@ print(dict1)
 
 print("\n*****")
 print("Exercise 10")
-print("a sequence of comma-separated numbers from console and generates a list and a tuple which contains every number in the form of string.")
+# a sequence of comma-separated numbers from console and generates a list and a tuple which contains every number in the form of string.
 
 seq1 = (input("Enter the sequence of comma-seperate numbers: "))
 print ("Sequence in list: ",seq1.split(","))
