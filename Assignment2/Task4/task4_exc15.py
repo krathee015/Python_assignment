14f6487fad65b875bcdd2ac6287ad74c61e046b9
Krathee <kanikarathee015@gmail.com>
2021-01-04 00:23:31 -0600

    added exc 15

---
+++ Assignment2/Task4/task4_exc15.py
@@ -0,0 +1,7 @@
+print("multiply the elements of a list by itself using a traditional function and pass the function to map() to complete the operation.")
+
+def multiply(x):
+    return x*x
+
+elem = map(multiply,[1,2,3,4,5,6])
+print(list(elem))
