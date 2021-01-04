eba865e3c6577842fe1d55c05a2d5083de2d1bd9
Krathee <kanikarathee015@gmail.com>
2021-01-03 20:26:14 -0600

    Updated exc 8 and added exc 10

---
+++ Assignment2/Task4/task4_exc10.py
@@ -0,0 +1,4 @@
+print("filter() to make a list whose elements are even numbers between 1 and 20 (both included)")
+
+lst = filter(lambda x:x%2 == 0, range(1,21))
+print(list(lst))
--- Assignment2/Task4/task4_exc8.py
+++ Assignment2/Task4/task4_exc8.py
@@ -1,10 +1,4 @@
 print("generate and print a tuple where the values are square of numbers between 1 and 20 (both 1 and 20 included)")
 
-def tup_squ():
-
-    t = list() #emply list, appending into it and converting to tuple
-    for i in range(1,21):
-        t.append(i**2)
-    print (tuple(t))
-
-tup_squ()
+lst = map(lambda x: x**2, range(1,21))
+print(tuple(lst))
