97c84fef51f5970379befb54207ebb2426433d56
Krathee <kanikarathee015@gmail.com>
2021-01-08 23:00:03 -0600

    adding exercise 2

--- Assignment4-task7/task7_exc2.py
+++ Assignment4-task7/task7_exc2.py
@@ -3,8 +3,22 @@
 # where Shape’s area is 0 by default.
 
 class Shape:
-    
-    class Square(Shape):
 
-        def __init__(self,length):
+    def area(self):
+        self.area_var = 0
+        print("area of shape is: ",self.area_var)
+
+class Square(Shape):
+    def __init__(self,length):
+        self.length = length
+    
+    def area(self):
+        area = (self.length * self.length)
+        print("The area of square with length {} is:{}".format(self.length,area))
+
+value_length = int(input("Enter the value of length: "))
+s_shape = Shape()
+s_shape.area()
+s_square = Square(value_length)
+s_square.area()
 
